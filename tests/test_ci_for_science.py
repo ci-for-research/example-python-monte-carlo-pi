b87c0760a49f94eac3cf593d59aa4384ff2322ea
felipez <tifonzafel@gmail.com>
2019-11-26 11:54:49 +0100

    changed test

--- tests/test_ci_for_science.py
+++ tests/test_ci_for_science.py
@@ -6,7 +6,8 @@ from ci_for_science import compute_pi
 
 def test_compute_pi():
     """Check that the computation approximates pi."""
-    approx_pi = compute_pi(10000)
+    approx_pi = compute_pi(100000)
+    print("Approximate value of pi is: ", approx_pi)
 
     predicate_1 = approx_pi < 3.15
     predicate_2 = approx_pi > 3.13
