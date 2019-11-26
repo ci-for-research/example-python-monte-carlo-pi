d8109240dcca2536f73400fb9569c8cfcdec84e5
felipez <tifonzafel@gmail.com>
2019-11-26 13:36:44 +0100

    trigger gitlabci

--- tests/test_ci_for_science.py
+++ tests/test_ci_for_science.py
@@ -9,7 +9,7 @@ def test_compute_pi():
     approx_pi = compute_pi(100000)
     print("Approximate value of pi is: ", approx_pi)
 
-    predicate_1 = approx_pi < 3.15
-    predicate_2 = approx_pi > 3.13
+    predicate_1 = approx_pi < 3.16
+    predicate_2 = approx_pi > 3.12
 
     assert predicate_1 and predicate_2
